984e79649e79a04746a6e67358b6b84796ef1b98
Christian <christianstewart5111@gmail.com>
2023-01-02 02:42:30 +0800

    Updated Main Flutter File

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:github/github.dart';
+
 import 'github_oauth_credentials.dart';
 import 'src/github_login.dart';
 
@@ -30,15 +32,22 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GithubLoginWidget(
       builder: (context, httpClient) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-          ),
-          body: const Center(
-            child: Text(
-              'You are logged in to GitHub!',
-            ),
-          ),
+        return FutureBuilder<CurrentUser>(
+          future: viewerDetail(httpClient.credentials.accessToken),
+          builder: (context, snapshot) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(title),
+              ),
+              body: Center(
+                child: Text(
+                  snapshot.hasData
+                      ? 'Hello ${snapshot.data!.login}!'
+                      : 'Retrieving viewer login details...',
+                ),
+              ),
+            );
+          },
         );
       },
       githubClientId: githubClientId,
@@ -46,4 +55,9 @@ class MyHomePage extends StatelessWidget {
       githubScopes: githubScopes,
     );
   }
+}
+
+Future<CurrentUser> viewerDetail(String accessToken) async {
+  final gitHub = GitHub(auth: Authentication.withToken(accessToken));
+  return gitHub.users.getCurrentUser();
 }
