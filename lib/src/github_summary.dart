7870086f3b5f2c51088d946b5b5996dff381ddfa
Christian <christianstewart5111@gmail.com>
2023-01-03 14:01:34 +0800

    Added Pull Requests

--- lib/src/github_summary.dart
+++ lib/src/github_summary.dart
@@ -161,6 +161,56 @@ String _nameWithOwner(Issue assignedIssue) {
   return assignedIssue.url.substring(29, endIndex);
 }
 
+class PullRequestsList extends StatefulWidget {
+  const PullRequestsList({required this.gitHub, super.key});
+  final GitHub gitHub;
+
+  @override
+  State<PullRequestsList> createState() => _PullRequestsListState();
+}
+
+class _PullRequestsListState extends State<PullRequestsList> {
+  @override
+  initState() {
+    // TODO: implement initState
+    super.initState();
+    _pullRequests = widget.gitHub.pullRequests.list(RepositorySlug('flutter', 'flutter')).toList();
+  }
+
+  late Future<List<PullRequest>> _pullRequests;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<PullRequest>>(
+      future: _pullRequests,
+      builder: (context, snapshot) {
+        if(snapshot.hasError) {
+          return Center(child: Text('${snapshot.error}'),);
+        }
+        if(!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator(),);
+        }
+
+        var pullRequests = snapshot.data;
+        return ListView.builder(
+          primary: false,
+          itemBuilder: (context, index) {
+            var pullRequest = pullRequests![index];
+            return ListTile(
+              title: Text(pullRequest.title ?? ''),
+              subtitle: Text('flutter/flutter ' 'PR #${pullRequest.number} ' 'opened by ${pullRequest.user?.login ?? ''}' '(${pullRequest.state?.toLowerCase() ?? ''})'),
+              onTap: () =>  _launchUrl(context, pullRequest.htmlUrl ?? ''),
+            );
+          },
+          itemCount: pullRequests!.length,
+        );
+      },
+    );
+  }
+}
+
+
+
 Future<void> _launchUrl(BuildContext context, String url) async {
   if (await canLaunchUrlString(url)) {
     await launchUrlString(url);
